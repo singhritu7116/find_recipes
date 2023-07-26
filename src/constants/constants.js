126855088665274a558ced2ca5c0c467fa5f924f
RITU KUMARI <102386249+singhritu7116@users.noreply.github.com>
2023-07-26 06:01:28 +0530

    Add files via upload

---
+++ src/constants/constants.js
@@ -0,0 +1,7 @@
+export const logo= require('./cooklogo.png');
+
+export const API_URL = 'https://forkify-api.herokuapp.com/api';
+
+export const pic1=require('./noodles.jpg');
+
+export const pic2=require('./plate.jpg')
